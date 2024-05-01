c4ab6055fbca5ccdb8efbf5b1f85f832d9eaee53
DanyFC <danyfarfanx@gmail.com>
2024-05-01 15:28:31 -0500

    feat: Added header with navigation
    Added header and navigation components with their styles, added global styles and removed irrelevant images from the project.

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,35 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { css } from "@emotion/react"
+import Navigation from "./navigation"
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <Link to="/">
-      <h2>
-        {siteTitle}
-      </h2>
-    </Link>
+  <header css={css`
+    background-color: #333;
+    padding: 1rem;
+  `}>
+    <div css={css`
+      max-width: 1200px;
+      margin: 0 auto;
+
+      @media (min-width: 768px) {
+        align-items: center;
+        display: flex;
+        justify-content: space-between;
+      }
+    `}>
+      <Link to="/" css={css`
+        text-decoration: none;
+      `}>
+        <h1 css={css`
+        color: #fff;
+        text-align: center;
+        `}>
+          {siteTitle}
+        </h1>
+      </Link>
+      <Navigation />
+    </div>
   </header>
 )
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -26,11 +26,32 @@ const Layout = ({ children }) => {
     <>
       <Global
         styles={css`
-        
+        html {
+          font-size: 62.5%;
+        }
+        body {
+          font-size: 1.6rem;
+          line-height: 1.5;
+        }
+        h1, h2,h3 {
+          margin: 0;
+          line-height: 1.5;
+        }
+        h1, h2 {
+          font-family: 'Roboto', sans-serif;
+        }
+        h3 {
+          font-family: 'PT Sans', sans-serif;
+        }
+        ul {
+          list-style: none;
+          margin: 0;
+          padding: 0;
+        }
       `}
       />
       <Helmet>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet"/>
       </Helmet>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div>
